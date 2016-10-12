3a22fc1b357e07a03d4817badbde23ba2af8d10b
Sinan <Sinan_Sagir@brown.edu>
2016-10-12 18:11:04 -0400

    fixes and combination

---
+++ combination/limits_9Oct16_tau21fix/X53X53_Combination_M700_RH.sh
@@ -0,0 +1,7 @@
+#!/bin/sh 
+cd /home/ssagir/CMSSW_7_3_0/src/
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cmsenv
+cd -
+cd limits_9Oct16_tau21fix/
+/home/ssagir/CMSSW_7_3_0/src/theta/utils2/theta-auto.py /home/ssagir/CMSSW_7_3_0/src/singleLepAnalyzer/x53x53_2015/combination/limits_9Oct16_tau21fix/X53X53_Combination_M700_RH.py
