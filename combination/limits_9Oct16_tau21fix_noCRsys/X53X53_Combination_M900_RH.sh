5380a3f5c189365933d378fa9ede9c0791453946
Sinan <Sinan_Sagir@brown.edu>
2016-10-12 18:15:08 -0400

    rm unnecessary files

--- combination/limits_9Oct16_tau21fix_noCRsys/X53X53_Combination_M900_RH.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
-cd /home/ssagir/CMSSW_7_3_0/src/
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-cmsenv
-cd -
-cd limits_9Oct16_tau21fix_noCRsys/
-/home/ssagir/CMSSW_7_3_0/src/theta/utils2/theta-auto.py /home/ssagir/CMSSW_7_3_0/src/singleLepAnalyzer/x53x53_2015/combination/limits_9Oct16_tau21fix_noCRsys/X53X53_Combination_M900_RH.py
