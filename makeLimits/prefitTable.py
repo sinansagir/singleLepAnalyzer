4fe333837a1044cc921297839b1bcd2926218e80
Sinan <Sinan_Sagir@brown.edu>
2016-10-15 12:00:59 -0400

    shape shift fix

--- makeLimits/prefitTable.py
+++ makeLimits/prefitTable.py
@@ -99,6 +99,7 @@ ewkInd = 1
 qcdInd = 2
 sigInd = 3
 topInd = 4
+procs = ['ewk','qcd','sig','top']
 prefitUncs = {}
 for ind in obsIndexes:
 	lowerBy = 0
@@ -108,69 +109,25 @@ for ind in obsIndexes:
 	for item in data: 
 		if 'process' in item or '/' in item or 'nuisance' in item or 'parameter' in item or 'gauss' in item: continue
 		prefitUncs[ind]['main'].append([item,'(gauss)'])
-	if prefitlines[ind+ewkInd-lowerBy].startswith('ewk'): 
-		data = prefitlines[ind+ewkInd-lowerBy].strip().split()
-		prefitUncs[ind]['ewk'] = []
-		for it in range(1,len(data)): 
-			if '(s)' in data[it] or '(r)' in data[it]: continue
-			if it==len(data)-1 and data[it]!='---': continue
-			if data[it]!='---': 
-				if data[it].startswith('pm'): prefitUncs[ind]['ewk'].append([data[it].replace('pm','$\pm$'),data[it+1]])
-				else:
-					for k in range(1,len(data[it])):
-						if not (data[it][k].isdigit() or data[it][k]=='.'): 
-							up = data[it][:k]
-							dn = data[it][k:]
-					prefitUncs[ind]['ewk'].append(['$^{'+up+'}_{'+dn+'}$',data[it+1]])
-			else: prefitUncs[ind]['ewk'].append([data[it].replace('---','-'),''])
-	else: lowerBy+=1
-	if prefitlines[ind+qcdInd-lowerBy].startswith('qcd'): 
-		data = prefitlines[ind+qcdInd-lowerBy].strip().split()
-		prefitUncs[ind]['qcd'] = []
-		for it in range(1,len(data)): 
-			if '(s)' in data[it] or '(r)' in data[it]: continue
-			if it==len(data)-1 and data[it]!='---': continue
-			if data[it]!='---': 
-				if data[it].startswith('pm'): prefitUncs[ind]['qcd'].append([data[it].replace('pm','$\pm$'),data[it+1]])
-				else:
-					for k in range(1,len(data[it])):
-						if not (data[it][k].isdigit() or data[it][k]=='.'): 
-							up = data[it][:k]
-							dn = data[it][k:]
-					prefitUncs[ind]['qcd'].append(['$^{'+up+'}_{'+dn+'}$',data[it+1]])
-			else: prefitUncs[ind]['qcd'].append([data[it].replace('---','-'),''])	
-	else: lowerBy+=1
-	if prefitlines[ind+sigInd-lowerBy].startswith('sig'): 
-		data = prefitlines[ind+sigInd-lowerBy].strip().split()
-		prefitUncs[ind]['sig'] = []
-		for it in range(1,len(data)): 
-			if '(s)' in data[it] or '(r)' in data[it]: continue
-			if it==len(data)-1 and data[it]!='---': continue
-			if data[it]!='---': 
-				if data[it].startswith('pm'): prefitUncs[ind]['sig'].append([data[it].replace('pm','$\pm$'),data[it+1]])
-				else:
-					for k in range(1,len(data[it])):
-						if not (data[it][k].isdigit() or data[it][k]=='.'): 
-							up = data[it][:k]
-							dn = data[it][k:]
-					prefitUncs[ind]['sig'].append(['$^{'+up+'}_{'+dn+'}$',data[it+1]])
-			else: prefitUncs[ind]['sig'].append([data[it].replace('---','-'),''])	
-	else: lowerBy+=1
-	if prefitlines[ind+topInd-lowerBy].startswith('top'): 
-		data = prefitlines[ind+topInd-lowerBy].strip().split()
-		prefitUncs[ind]['top'] = []
-		for it in range(1,len(data)): 
-			if '(s)' in data[it] or '(r)' in data[it]: continue
-			if it==len(data)-1 and data[it]!='---': continue
-			if data[it]!='---': 
-				if data[it].startswith('pm'): prefitUncs[ind]['top'].append([data[it].replace('pm','$\pm$'),data[it+1]])
-				else:
-					for k in range(1,len(data[it])):
-						if not (data[it][k].isdigit() or data[it][k]=='.'): 
-							up = data[it][:k]
-							dn = data[it][k:]
-					prefitUncs[ind]['top'].append(['$^{'+up+'}_{'+dn+'}$',data[it+1]])
-			else: prefitUncs[ind]['top'].append([data[it].replace('---','-'),''])
+	procInd = 0
+	for proc in procs:
+		procInd+=1
+		if prefitlines[ind+procInd-lowerBy].startswith(proc): 
+			data = prefitlines[ind+procInd-lowerBy].strip().split()
+			prefitUncs[ind][proc] = []
+			for it in range(1,len(data)): 
+				if '(s)' in data[it] or '(r)' in data[it]: continue
+				if it==len(data)-1 and data[it]!='---': continue
+				if data[it]!='---': 
+					if data[it].startswith('pm'): prefitUncs[ind][proc].append([data[it].replace('pm','$\pm$'),data[it+1]])
+					else:
+						for k in range(1,len(data[it])):
+							if not (data[it][k].isdigit() or data[it][k]=='.'): 
+								up = data[it][:k]
+								dn = data[it][k:]
+						prefitUncs[ind][proc].append(['$^{'+up+'}_{'+dn+'}$',data[it+1]])
+				else: prefitUncs[ind][proc].append([data[it].replace('---','-'),''])
+		else: lowerBy+=1
 
 nCat = 1
 for ind in obsIndexes:
@@ -179,10 +136,10 @@ for ind in obsIndexes:
 	print "\\topcaption{Pre-fit uncertainties in the",
 	if "isE" in observables[obsIndexes.index(ind)]: print "electron channel with",
 	if "isM" in observables[obsIndexes.index(ind)]: print "muon channel with",
-	if "nT0" in observables[obsIndexes.index(ind)]: print "0 top tag and",
-	if "nT1p" in observables[obsIndexes.index(ind)]: print "1 or more top tag and",
-	if "nW0" in observables[obsIndexes.index(ind)]: print "0 W tag and",
-	if "nW1p" in observables[obsIndexes.index(ind)]: print "1 or more W tag and",
+	if "nT0" in observables[obsIndexes.index(ind)]: print "0 top tag,",
+	if "nT1p" in observables[obsIndexes.index(ind)]: print "1 or more top tag,",
+	if "nW0" in observables[obsIndexes.index(ind)]: print "0 W tag, and",
+	if "nW1p" in observables[obsIndexes.index(ind)]: print "1 or more W tag, and",
 	if "nB0" in observables[obsIndexes.index(ind)]: print "0 b tag.}"
 	if "nB1" in observables[obsIndexes.index(ind)]: print "1 b tag.}"
 	if "nB2p" in observables[obsIndexes.index(ind)]: print "2 or more b tag.}"
