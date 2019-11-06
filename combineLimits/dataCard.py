d69bce65c976d28470b0ac229d59a1c2f034f18e
Sinan <Sinan_Sagir@brown.edu>
2019-11-06 15:09:46 -0500

    combine working setup with Harvester

--- combineLimits/dataCard.py
+++ combineLimits/dataCard.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
+import os,sys,time,math,datetime,itertools
+from ROOT import gROOT,TFile,TH1F
 import CombineHarvester.CombineTools.ch as ch
-import os
+parent = os.path.dirname(os.getcwd())
+sys.path.append(parent)
+from utils import *
 
+gROOT.SetBatch(1)
 
-def add_processes_and_observations(cb, prefix="x53x53right"):
+fileDir = "/user_data/ssagir/CMSSW_10_2_10/src/singleLepAnalyzer/fourtops/makeTemplates/"
+template = "noHOTtW_OR_onlyHOTtW_2019_10_24"
+saveKey = ""
+
+def add_processes_and_observations(cb, prefix="TTTT"):
     print '>> Creating processes and observations...'
-    if chiral=='LH': prefix="x53x53left"
     for chn in chns:
         cats_chn = cats[chn]
         if 'isCR' not in chn: 
@@ -21,12 +29,13 @@ def add_processes_and_observations(cb, prefix="x53x53right"):
 def add_shapes(cb):
     print '>> Extracting histograms from input root files...'
     for chn in chns:
-        file = os.environ['CMSSW_BASE'] + "/src/x53x53/master.root"
-        bkg_pattern = "minMlb_2p318fb_%s_$BIN__$PROCESS" % chn
+        #file = os.environ['CMSSW_BASE'] + "/src/x53x53/master.root"
+        file = fileDir+"templates_"+template+"/templates_HT_41p53fb_rebinned_stat0p3.root"
+        bkg_pattern = "HT_41p53fb_%s$BIN__$PROCESS" % chn
         cb.cp().channel([chn]).era([era]).backgrounds().ExtractShapes(
             file, bkg_pattern, bkg_pattern + "__$SYSTEMATIC")
 
-        sig_pattern = "minMlb_2p318fb_%s_$BIN__$PROCESS$MASS" % chn
+        sig_pattern = "HT_41p53fb_%s$BIN__$PROCESS$MASS" % chn
         if 'isCR' not in chn:
 			cb.cp().channel([chn]).era([era]).signals().ExtractShapes(
 				file, sig_pattern, sig_pattern + "__$SYSTEMATIC")
@@ -43,15 +52,15 @@ def add_bbb(cb):
         	bbb.MergeAndAdd(cb_chn.cp().era([era]).bin_id([0,1,2,3]).process(bkg_procs[chn]), cb)
         	bbb.MergeAndAdd(cb_chn.cp().era([era]).bin_id([0,1,2,3]).process(sig_procs), cb)
         else:
-        	bbb.MergeAndAdd(cb_chn.cp().era([era]).bin_id([0,1,2,3,4,5,6,7]).process(bkg_procs[chn]), cb)
-        	bbb.MergeAndAdd(cb_chn.cp().era([era]).bin_id([0,1,2,3,4,5,6,7]).process(sig_procs), cb)			
+        	bbb.MergeAndAdd(cb_chn.cp().era([era]).bin_id([0]).process(bkg_procs[chn]), cb)
+        	bbb.MergeAndAdd(cb_chn.cp().era([era]).bin_id([0]).process(sig_procs), cb)			
 
 def rename_and_write(cb):
     print '>> Setting standardised bin names...'
     ch.SetStandardBinNames(cb)
 
-    writer = ch.CardWriter('LIMITS_'+chiral+'/$TAG/$MASS/$ANALYSIS_$CHANNEL_$BINID_$ERA.txt',
-                           'LIMITS_'+chiral+'/$TAG/common/$ANALYSIS_$CHANNEL.input.root')
+    writer = ch.CardWriter('limits_'+template+saveKey+'/$TAG/$MASS/$ANALYSIS_$CHANNEL_$BINID_$ERA.txt',
+                           'limits_'+template+saveKey+'/$TAG/common/$ANALYSIS_$CHANNEL.input.root')
 
     writer.SetVerbosity(1)
     writer.WriteCards('cmb', cb)
@@ -72,41 +81,63 @@ def add_systematics(cb):
     print '>> Adding systematic uncertainties...'
 
     signal = cb.cp().signals().process_set()
-    bkg = ['ewk', 'top', 'qcd']
-    bkgNoQCD = ['ewk', 'top']
+    bkg = ['ttbb', 'ttcc', 'ttjj', 'top', 'ewk', 'qcd']
+    bkgNoQCD = ['ttbb', 'ttcc', 'ttjj', 'top', 'ewk']
 
-    cb.cp().process(signal + bkg).channel(['isSR_isE','isSR_isM','isCR_isE','isCR_isM']).AddSyst(cb, "lumi", "lnN", ch.SystMap()(1.027))
-    cb.cp().process(signal + bkg).channel(['isSR_isE','isCR_isE']).AddSyst(cb, "elTrigSys", "lnN", ch.SystMap()(1.05))
-    cb.cp().process(signal + bkg).channel(['isSR_isM','isCR_isM']).AddSyst(cb, "muTrigSys", "lnN", ch.SystMap()(1.05))
-    cb.cp().process(signal + bkg).channel(['isSR_isE','isCR_isE']).AddSyst(cb, "elIdSys", "lnN", ch.SystMap()(1.01))
-    cb.cp().process(signal + bkg).channel(['isSR_isM','isCR_isM']).AddSyst(cb, "muIdSys", "lnN", ch.SystMap()(1.01))
-    cb.cp().process(signal + bkg).channel(['isSR_isE','isCR_isE']).AddSyst(cb, "elIsoSys", "lnN", ch.SystMap()(1.01))
-    cb.cp().process(signal + bkg).channel(['isSR_isM','isCR_isM']).AddSyst(cb, "muIsoSys", "lnN", ch.SystMap()(1.01))
-    cb.cp().process(signal + bkg).channel(['isSR_isE','isSR_isM']).AddSyst(cb, "pileup", "shape", ch.SystMap()(1.0))
-    cb.cp().process(signal + bkg).channel(['isSR_isE','isSR_isM']).AddSyst(cb, "jec", "shape", ch.SystMap()(1.0))
-    cb.cp().process(signal + bkg).channel(['isSR_isE','isSR_isM']).AddSyst(cb, "jer", "shape", ch.SystMap()(1.0))
-    cb.cp().process(signal + bkg).channel(['isSR_isE','isSR_isM']).AddSyst(cb, "btag", "shape", ch.SystMap()(1.0))
-    cb.cp().process(signal + bkg).channel(['isSR_isE','isSR_isM']).AddSyst(cb, "mistag", "shape", ch.SystMap()(1.0))
-    cb.cp().process(signal + bkgNoQCD).channel(['isSR_isE','isSR_isM']).AddSyst(cb, "jmr", "shape", ch.SystMap()(1.0))
-    cb.cp().process(signal + bkgNoQCD).channel(['isSR_isE','isSR_isM']).AddSyst(cb, "jms", "shape", ch.SystMap()(1.0))
-    cb.cp().process(signal + bkgNoQCD).channel(['isSR_isE','isSR_isM']).AddSyst(cb, "tau21", "shape", ch.SystMap()(1.0))
-    cb.cp().process(signal + bkgNoQCD).channel(['isSR_isE','isSR_isM']).AddSyst(cb, "topsf", "shape", ch.SystMap()(1.0))
-    cb.cp().process(['top']).channel(['isSR_isE','isSR_isM']).AddSyst(cb, "toppt", "shape", ch.SystMap()(1.0))
-    cb.cp().process(['top']).channel(['isSR_isE','isSR_isM']).AddSyst(cb, "q2", "shape", ch.SystMap()(1.0))
-    cb.cp().process(signal + bkg).channel(['isSR_isE','isSR_isM','isCR_isE','isCR_isM']).AddSyst(cb, "muRFcorrdNew", "shape", ch.SystMap()(1.0))
-    cb.cp().process(signal + bkg).channel(['isSR_isE','isSR_isM','isCR_isE','isCR_isM']).AddSyst(cb, "pdfNew", "shape", ch.SystMap()(1.0))
-    cb.cp().process(['ewk']).channel(['isSR_isE','isCR_isE']).AddSyst(cb, "ewk_SF_el", "lnU", ch.SystMap()(100.0))
-    cb.cp().process(['ewk']).channel(['isSR_isM','isCR_isM']).AddSyst(cb, "ewk_SF_mu", "lnU", ch.SystMap()(100.0))
-    cb.cp().process(['top']).channel(['isSR_isE','isCR_isE']).AddSyst(cb, "top_SF_el", "lnU", ch.SystMap()(100.0))
-    cb.cp().process(['top']).channel(['isSR_isM','isCR_isM']).AddSyst(cb, "top_SF_mu", "lnU", ch.SystMap()(100.0))
+#     cb.cp().process(signal + bkg).channel(['isSR_isE','isSR_isM','isCR_isE','isCR_isM']).AddSyst(cb, "lumi", "lnN", ch.SystMap()(1.027))
+#     cb.cp().process(signal + bkg).channel(['isSR_isE','isCR_isE']).AddSyst(cb, "elTrigSys", "lnN", ch.SystMap()(1.05))
+#     cb.cp().process(signal + bkg).channel(['isSR_isM','isCR_isM']).AddSyst(cb, "muTrigSys", "lnN", ch.SystMap()(1.05))
+#     cb.cp().process(signal + bkg).channel(['isSR_isE','isCR_isE']).AddSyst(cb, "elIdSys", "lnN", ch.SystMap()(1.01))
+#     cb.cp().process(signal + bkg).channel(['isSR_isM','isCR_isM']).AddSyst(cb, "muIdSys", "lnN", ch.SystMap()(1.01))
+#     cb.cp().process(signal + bkg).channel(['isSR_isE','isCR_isE']).AddSyst(cb, "elIsoSys", "lnN", ch.SystMap()(1.01))
+#     cb.cp().process(signal + bkg).channel(['isSR_isM','isCR_isM']).AddSyst(cb, "muIsoSys", "lnN", ch.SystMap()(1.01))
+    cb.cp().process(signal + bkg).channel(chns).AddSyst(cb, "lumi", "lnN", ch.SystMap()(1.023))
+    cb.cp().process(signal + bkg).channel(chns).AddSyst(cb, "jec", "shape", ch.SystMap()(1.0))
+    cb.cp().process(signal + bkg).channel(chns).AddSyst(cb, "jer", "shape", ch.SystMap()(1.0))
+    cb.cp().process(signal + bkg).channel(chns).AddSyst(cb, "prefire", "shape", ch.SystMap()(1.0))
+    cb.cp().process(signal + bkg).channel(chns).AddSyst(cb, "pileup", "shape", ch.SystMap()(1.0))
+    cb.cp().process(['ttbb']).channel(chns).AddSyst(cb, "ttbbmuRFcorrdNew", "shape", ch.SystMap()(1.0))
+    cb.cp().process(['ttcc']).channel(chns).AddSyst(cb, "ttccmuRFcorrdNew", "shape", ch.SystMap()(1.0))
+    cb.cp().process(['ttjj']).channel(chns).AddSyst(cb, "ttjjmuRFcorrdNew", "shape", ch.SystMap()(1.0))
+    cb.cp().process(['top']).channel(chns).AddSyst(cb, "topmuRFcorrdNew", "shape", ch.SystMap()(1.0))
+    cb.cp().process(['ewk']).channel(chns).AddSyst(cb, "ewkmuRFcorrdNew", "shape", ch.SystMap()(1.0))
+    cb.cp().process(['qcd']).channel(chns).AddSyst(cb, "qcdmuRFcorrdNew", "shape", ch.SystMap()(1.0))
+    cb.cp().process(signal).channel(chns).AddSyst(cb, "TTTTM690muRFcorrdNew", "shape", ch.SystMap()(1.0))
+#     cb.cp().process(signal + bkg).channel(chns).AddSyst(cb, "pdfNew", "shape", ch.SystMap()(1.0))
+#     cb.cp().process(['ttbb', 'ttcc', 'ttjj']).channel(chns).AddSyst(cb, "toppt", "shape", ch.SystMap()(1.0))
+#     cb.cp().process(signal + bkg).channel(chns).AddSyst(cb, "tau32", "shape", ch.SystMap()(1.0))
+#     cb.cp().process(signal + bkg).channel(chns).AddSyst(cb, "jmst", "shape", ch.SystMap()(1.0))
+#     cb.cp().process(signal + bkg).channel(chns).AddSyst(cb, "jmrt", "shape", ch.SystMap()(1.0))
+#     cb.cp().process(signal + bkg).channel(chns).AddSyst(cb, "tau21", "shape", ch.SystMap()(1.0))
+#     cb.cp().process(signal + bkg).channel(chns).AddSyst(cb, "jmsW", "shape", ch.SystMap()(1.0))
+#     cb.cp().process(signal + bkg).channel(chns).AddSyst(cb, "jmrW", "shape", ch.SystMap()(1.0))
+#     cb.cp().process(signal + bkg).channel(chns).AddSyst(cb, "tau21pt", "shape", ch.SystMap()(1.0))
+    cb.cp().process(signal + bkg).channel(chns).AddSyst(cb, "btag", "shape", ch.SystMap()(1.0))
+    cb.cp().process(signal + bkg).channel(chns).AddSyst(cb, "mistag", "shape", ch.SystMap()(1.0))
 
+def add_autoMCstat(cb):
+    print '>> Adding autoMCstats...'
 
+    thisDir = os.getcwd()
+    for chn in chns+['cmb']:
+    	chnDir = os.getcwd()+'/limits_'+template+saveKey+'/'+chn+'/690/'
+    	files = [x for x in os.listdir(chnDir) if '.txt' in x]
+    	os.chdir(chnDir)
+    	for thefile in files:
+    		with open(chnDir+thefile, "a") as chnfile: chnfile.write("* autoMCStats 1.")
+    		print "*"*20
+    		print "Making workspace from", chnDir+thefile
+    		cmd = "text2workspace.py "+chnDir+thefile+" -v 1 -m 690 --no-b-only"
+    		os.system(cmd)
+    	os.chdir(thisDir)
+    
 def go(cb):
     add_processes_and_observations(cb)
     add_systematics(cb)
     add_shapes(cb)
-    add_bbb(cb)
+    #add_bbb(cb)
     rename_and_write(cb)
+    add_autoMCstat(cb)
     # print_cb(cb)
 
 
@@ -115,48 +146,20 @@ if __name__ == "__main__":
     # cb.SetVerbosity(20)
 
     era = '13TeV'
-    chiral = 'RH'
+    
+    tfile = TFile(fileDir+"templates_"+template+"/templates_HT_41p53fb_rebinned_stat0p3.root")
+    allHistNames = [k.GetName() for k in tfile.GetListOfKeys() if not (k.GetName().endswith('Up') or k.GetName().endswith('Down'))]
+    tfile.Close()
+    dataName = 'data_obs'
+    chns = [hist[hist.find('fb_')+3:hist.find('__')] for hist in allHistNames if '__'+dataName in hist]
+    bkg_procs = {chn:[hist.split('__')[-1] for hist in allHistNames if chn in hist and not (hist.endswith('Up') or hist.endswith('Down') or hist.endswith(dataName) or hist.endswith('TTTTM690'))] for chn in chns}
+    print bkg_procs
 
-    bkg_procs = {'isSR_isE' : ['ewk', 'top', 'qcd'],
-                 'isSR_isM' : ['ewk', 'top', 'qcd'],
-                 'isCR_isE' : ['ewk', 'top', 'qcd'],
-                 'isCR_isM' : ['ewk', 'top', 'qcd'],
-                 }
-    chns = sorted(bkg_procs.keys())
+    sig_procs = ['TTTTM']
+    
+    cats = {}
+    for chn in chns: cats[chn] = [(0, '')]
 
-    if chiral=='RH': sig_procs = ['X53X53rightM']
-    if chiral=='LH': sig_procs = ['X53X53leftM']
-
-    cats = {'isSR_isE' :[(0, 'nT0_nW0_nB1'),
-						 (1, 'nT0_nW0_nB2p'),
-						 (2, 'nT0_nW1p_nB1'),
-						 (3, 'nT0_nW1p_nB2p'),
-						 (4, 'nT1p_nW0_nB1'),
-						 (5, 'nT1p_nW0_nB2p'),
-						 (6, 'nT1p_nW1p_nB1'),
-						 (7, 'nT1p_nW1p_nB2p'),
-						 ],
-			'isSR_isM' :[(0, 'nT0_nW0_nB1'),
-						 (1, 'nT0_nW0_nB2p'),
-						 (2, 'nT0_nW1p_nB1'),
-						 (3, 'nT0_nW1p_nB2p'),
-						 (4, 'nT1p_nW0_nB1'),
-						 (5, 'nT1p_nW0_nB2p'),
-						 (6, 'nT1p_nW1p_nB1'),
-						 (7, 'nT1p_nW1p_nB2p'),
-						 ],
-            'isCR_isE' :[(0, 'nT0p_nW0p_nB1'),
-						 (1, 'nT0p_nW0p_nB2p'),
-						 (2, 'nT0p_nW0_nB0'),
-						 (3, 'nT0p_nW1p_nB0'),
-						 ],
-			'isCR_isM' :[(0, 'nT0p_nW0p_nB1'),
-						 (1, 'nT0p_nW0p_nB2p'),
-						 (2, 'nT0p_nW0_nB0'),
-						 (3, 'nT0p_nW1p_nB0'),
-						],
-            }
-
-    masses = ch.ValsFromRange('700:1600|100')
+    masses = ch.ValsFromRange('690')
     go(cb)
 
---
+++ combineLimits/printResults.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import os,copy,math,glob
+
+inputDir="limits_noHOTtW_OR_onlyHOTtW_2019_10_24/"
+
+limDirsNoJet = [tag for tag in sorted(os.listdir(inputDir)) if '_nJ6' in tag]
+print len(limDirsNoJet)*5
+
+for inputPathh in sorted(os.listdir(inputDir)):
+	if 'isSR' not in inputPathh: continue
+	if '_nJ6' not in inputPathh: continue
+	for nJet in ['_nJ5','_nJ6','_nJ7','_nJ8','_nJ9','_nJ10p']:
+		inputPath = inputPathh.replace('_nJ6',nJet)
+		sigFile = inputDir+inputPath+'/690/Significance.txt'
+		sigData = open(sigFile,'r').read()
+		siglines = sigData.split('\n')
+		limFile = inputDir+inputPath+'/690/AsymptoticLimits.txt'
+		limData = open(limFile,'r').read()
+		limlines = limData.split('\n')
+		theSig = ''
+		theLim = ''
+		for line in siglines:
+			if line.startswith('Significance:'): theSig = line.split()[-1]
+		for line in limlines:
+			if line.startswith('Expected 50.0%:'): theLim = line.split()[-1]
+		print inputPath,theSig,theLim
