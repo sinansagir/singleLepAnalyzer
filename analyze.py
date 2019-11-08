19fbc5975c5a683eb063f9e5ee608f41d630e331
Sinan <Sinan_Sagir@brown.edu>
2019-11-08 10:44:24 -0500

    toppt systematic

--- analyze.py
+++ analyze.py
@@ -92,8 +92,8 @@ def analyze(tTree,process,flv,cutList,doAllSys,doJetRwt,iPlot,plotDetails,catego
 		weightmuRDownStr    = 'renormWeights[2] * '+weightStr
 		weightmuFUpStr      = 'renormWeights[1] * '+weightStr
 		weightmuFDownStr    = 'renormWeights[0] * '+weightStr
-		weighttopptUpStr    = weightStr.replace(topPt13TeVstr,'1')
-		weighttopptDownStr  = weightStr #.replace('topPtWeight13TeV','topPtWeight13TeV*topPtWeight13TeV')
+		weighttopptUpStr    = '('+topPt13TeVstr+') * '+weightStr
+		weighttopptDownStr  = '(1/'+topPt13TeVstr+') * '+weightStr
 		weightjsfUpStr      = weightStr.replace(jetSFstr,jetSFstrUp)
 		weightjsfDownStr    = weightStr.replace(jetSFstr,jetSFstrDn)
 
