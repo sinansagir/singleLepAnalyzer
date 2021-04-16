5988ac62bc7cf9943ff279191a3190a64fda9686
Sinan Sagir <Sinan.Sagir@cern.ch>
2021-04-16 19:01:02 -0400

    unimportant updates

--- plotShapeShifts/plotSystsSigCombo.py
+++ plotShapeShifts/plotSystsSigCombo.py
@@ -14,7 +14,7 @@ rt.gROOT.SetBatch(1)
 
 outDir = os.getcwd()+'/'
 iPlot = sys.argv[1]#'HT'
-year = 'R18'
+year = 'R16'
 if year=='R16':
 	lumiStr = '35p867fb'
 	lumi=35.9 #for plots
@@ -26,7 +26,7 @@ elif year=='R18':
 	lumi=59.97 #for plots
 sig1 = 'tttt' #  choose the 1st signal to plot
 useCombine = True
-tempVersion = 'templates_'+year+'_2021_3_8'
+tempVersion = 'templates_'+year+'_2021_4_6'
 isRebinned = '_rebinned_stat0p3'
 if 'kinematics' in tempVersion: isRebinned = '_rebinned_stat1p1'
 cutString = ''
@@ -45,7 +45,7 @@ if 'kinematics' in tempVersion:
 	nhottlist = ['0p']
 	nbtaglist = ['2p']
 	njetslist = ['4p']
-systematics = ['pileup','btag','mistag','hotstat','hotcspur','hotclosure','isr','fsr','PSwgt','muRF','pdf']#,'hdamp','ue','ht','trigeff','toppt','tau32','jmst','jmrt','tau21','jmsW','jmrW','tau21pt'] #
+systematics = ['pileup','btag','btagcorr','btaguncorr','mistag','hotstat','hotcspur','hotclosure','isr','fsr','PSwgt','muRF','pdf']#,'hdamp','ue','ht','trigeff','toppt','tau32','jmst','jmrt','tau21','jmsW','jmrW','tau21pt'] #
 if year!='R18': systematics += ['prefire']
 # if year=='R18': systematics += ['hem']
 systematics+= ['JEC','JER']#,
@@ -55,6 +55,7 @@ systematics+= ['JEC','JER']#,
 # 'JEC_HF','JEC_HF_'+year.replace('R','20'),
 # 'JEC_EC2','JEC_EC2_'+year.replace('R','20'),
 # 'JEC_BBEC1','JEC_BBEC1_'+year.replace('R','20')]
+#systematics = ['btag','btagcorr','btaguncorr','mistag']
 systematics = ['lowess'+syst for syst in systematics]
 
 signameList = ['tttt']
